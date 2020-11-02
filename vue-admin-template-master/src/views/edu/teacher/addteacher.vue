a93d2ee55380ffbc4fc363f0a7bf564087a2bcf5
Levi <291524262@qq.com>
2020-11-02 10:01:41 +0800

    填写校验

--- vue-admin-template-master/src/views/edu/teacher/addteacher.vue
+++ vue-admin-template-master/src/views/edu/teacher/addteacher.vue
@@ -1,27 +1,33 @@
 <template>
   <div class="app-container">
-    <el-form label-width="120px">
-      <el-form-item label="讲师名称">
+    <el-form
+      :model="teacher"
+      :rules="rules"
+      ref="teacher"
+      label-width="120px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="讲师名称" prop="name">
         <el-input v-model="teacher.name" />
       </el-form-item>
-      <el-form-item label="讲师排序">
+      <el-form-item label="讲师排序" prop="sort">
         <el-input-number
           v-model="teacher.sort"
           controls-position="right"
           :min="0"
         />
       </el-form-item>
-      <el-form-item label="讲师头衔">
+      <el-form-item label="讲师头衔" prop="level">
         <el-select v-model="teacher.level" clearable placeholder="请选择">
           <!--数据类型一定要和取出的json中的一致，否则没法,回填因此，这里value使用动态绑定的值，保证其数据类型是number-->
           <el-option :value="1" label="高级讲师" />
           <el-option :value="2" label="首席讲师" />
         </el-select>
       </el-form-item>
-      <el-form-item label="讲师资历">
+      <el-form-item label="讲师资历" prop="career">
         <el-input v-model="teacher.career" />
       </el-form-item>
-      <el-form-item label="讲师简介">
+      <el-form-item label="讲师简介" prop="intro">
         <el-input v-model="teacher.intro" :rows="10" type="textarea" />
       </el-form-item>
       <!-- 讲师头像：TODO -->
@@ -29,7 +35,7 @@
         <el-button
           :disabled="saveBtnDisabled"
           type="primary"
-          @click="addNewTeacher"
+          @click="submitForm('teacher')"
           >保存</el-button
         >
       </el-form-item>
@@ -52,14 +58,36 @@ export default {
         avatar: "",
       },
 
+      rules: {
+        name: [
+          { required: true, message: "请输入讲师名称", trigger: "blur" },
+          {
+            min: 2,
+            max: 10,
+            message: "长度在 2 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        sort: [{ required: true, message: "请选择排序", trigger: "change" }],
+        level: [{ required: true, message: "请选择等级", trigger: "change" }],
+        career: [{ required: true, message: "讲师资历", trigger: "change" }],
+        intro: [{ required: true, message: "讲师简介", trigger: "change" }],
+      },
+
       saveBtnDisabled: false, //保存按钮是否禁用
     };
   },
 
   methods: {
-    addNewTeacher() {
-      this.saveBtnDisabled = true;
-      this.saveData();
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.saveBtnDisabled = true;
+          this.saveData();
+        } else {
+          return false;
+        }
+      });
     },
 
     saveData() {
